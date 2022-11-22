58bc3a786f5ca10f6cbec11195964d9c19d4a77f
GET Academy <terje@getacademy.no>
2022-11-22 10:32:28 +0100

    Initial commit

---
+++ CountCharsRefactoring/App.cs
@@ -0,0 +1,50 @@
+﻿namespace CountCharsRefactoring
+{
+    /*
+     * 1: Innføre variabel
+     * 2: Innføre funksjon/metode
+     * 3: Innføre klasse
+     */
+    internal class App
+    {
+        public static void Run()
+        {
+            var range = 250;
+            var counts = new int[range];
+            while (true)
+            {
+                var text = Console.ReadLine();
+                CountCharacters(text, counts);
+                ShowCharactersAndCounts(range, counts);
+            }
+        }
+
+        private static void ShowCharactersAndCounts(int range, int[] counts)
+        {
+            for (var characterAsciiNumber = 0; characterAsciiNumber < range; characterAsciiNumber++)
+            {
+                var howManyOfThisCharacter = counts[characterAsciiNumber];
+                if (howManyOfThisCharacter > 0)
+                {
+                    var character = (char) characterAsciiNumber;
+                    Console.WriteLine(character + " - " + howManyOfThisCharacter);
+                }
+            }
+        }
+
+        private static void CountCharacters(string? text, int[] counts)
+        {
+            foreach (var character in text)
+            {
+                var characterAsciiNumber = ConvertCharacterToNumber(character);
+                counts[characterAsciiNumber]++;
+            }
+        }
+
+        private static int ConvertCharacterToNumber(char character)
+        {
+            var characterAsciiNumber = (int) character;
+            return characterAsciiNumber;
+        }
+    }
+}
---
+++ CountCharsRefactoring/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * 1: Innføre variabel
+ * 2: Innføre funksjon/metode
+ * 3: Innføre klasse
+ */
+
+var range = 250;
+var counts = new int[range];
+while (true)
+{
+    var text = Console.ReadLine();
+    foreach (var character in text)
+    {
+        var characterAsciiNumber = (int)character;
+        counts[characterAsciiNumber]++;
+    }
+    for (var characterAsciiNumber = 0; characterAsciiNumber < range; characterAsciiNumber++)
+    {
+        var howManyOfThisCharacter = counts[characterAsciiNumber];
+        if (howManyOfThisCharacter > 0)
+        {
+            var character = (char)characterAsciiNumber;
+            Console.WriteLine(character + " - " + howManyOfThisCharacter);
+        }
+    }
+}
+
+// DRY = Don't Repeat Yourself
+/*
+var a = 2;
+var b = 7;
+var myBase = a * b/ 723;
+var x = myBase + 14;
+var y = myBase - 39;
+var z = 2 * myBase;
+*/
